20bf0169ecaf714247175168bcc80e7e17037e51
Mohammad Saadat <mohammad2682593@gmail.com>
2019-05-15 23:48:27 +0430

    changes in sidenav and header component theme
    made some changes in sidenav component and header component based on sketches

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,17 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class HeaderComponent implements OnInit {
   @Input() sidenav: MatSidenav;
-  ngOnInit() {}
+  searchbar: boolean = false;
+
+  ngOnInit() { }
+
+  showSearchBar() {
+    this.searchbar = true;
+  }
+
+  hideSearchBar() {
+    // console.log($('#searchbar'));
+    // $('#searchbar').transition('side right');
+    this.searchbar = false;
+  }
 }
